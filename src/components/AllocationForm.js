041e6b6ad449d70e229605800fd9eb59ebe99bf1
PhFranca <phlavio.franca@outlook.com>
2023-05-28 11:48:31 -0400

    Final Project: adjustments on the AllocationForm to make currency visible and changing

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -58,8 +58,8 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                         <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
-                    <input
+                  <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', marginLeft: '2rem' }}>{currency}</span>
+                   <input
                         required='required'
                         type='number'
                         id='cost'
