47383639aae51c990a9cc37001e0727d1d875dc2
PhFranca <phlavio.franca@outlook.com>
2023-05-28 19:29:17 -0400

    Final Project: Add Currency.css file and some improvments

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,37 +1,54 @@
 import React, { useContext } from 'react';
+import Select from 'react-select';
 import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
   const { dispatch } = useContext(AppContext);
 
-    const changeCurrency = (val)=>{
-            dispatch({
-                type: 'CHG_CURRENCY',
-                payload: val,
-            })
-    }
-    
+  const options = [
+    { value: '$', label: '$ Dolar' },
+    { value: '£', label: '£ Pound' },
+    { value: '€', label: '€ Euro' },
+    { value: '₹', label: '₹ Ruppee' },
+    { value: 'R$', label: 'R$ Real' },
+  ];
+
+  const changeCurrency = (selectedOption) => {
+    dispatch({
+      type: 'CHG_CURRENCY',
+      payload: selectedOption.value,
+    });
+  };
+
   return (
-        <div className='alert alert-secondary'style={{ backgroundColor: "#61d172", color: 'white' }}> Currency {
-      <select name="Currency" id="Currency" 
-        style={{
-            backgroundColor: "#61d172",
+    <div className="col-sm alert select-container" style={{ display: 'flex', alignItems: 'center', backgroundColor: '#61d172', color: 'white' }}>
+      Currency:{' '}
+      <Select
+        options={options}
+        styles={{
+          control: (provided, state) => ({
+            ...provided,
+            backgroundColor: '#61d172',
             color: 'white',
             border: 'none',
             outline: 'none',
-            cursor: 'pointer',
-            borderRadius: '8px',
-        }} 
-      onChange={event=>changeCurrency(event.target.value)}>
-        <option value="$" style={{ backgroundColor: "#61d172", color: '#5b5b5b' }}>$ Dolar</option>
-        <option value="£" style={{ backgroundColor: "#61d172", color: '#5b5b5b' }}>£ Pound</option>
-        <option value="€" style={{ backgroundColor: "#61d172", color: '#5b5b5b' }}>€ Euro</option>
-        <option value="₹" style={{ backgroundColor: "#61d172", color: '#5b5b5b' }}>₹ Ruppee</option>
-        <option value="R$" style={{ backgroundColor: "#61d172", color: '#5b5b5b' }}>R$ Real</option>
-      </select>	
-      }	
+            borderRadius: '10px',
+          }),
+          singleValue: (provided) => ({
+            ...provided,
+            color: 'white',
+          }),
+          option: (provided, state) => ({
+            ...provided,
+            backgroundColor: state.isFocused ? 'white' : '#61d172',
+            color: state.isFocused ? 'green' : '#5b5b5b',
+          }),
+        }}
+        onChange={changeCurrency}
+        defaultValue={options.find((option) => option.value === '$')}
+      />
     </div>
-    );
+  );
 };
 
 export default Currency;
